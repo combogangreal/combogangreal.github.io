05cbb9e1f660a9ba101c61c1b35a554c6e0d67ac
Xero Runs <150632490+combogangreal@users.noreply.github.com>
2023-11-18 21:03:02 -0800

    commit

---
+++ portfolio.ts
@@ -0,0 +1,220 @@
+import emoji from 'react-easy-emoji';
+import {
+  EducationType,
+  ExperienceType,
+  FeedbackType,
+  ProjectType,
+  SkillsSectionType,
+  SkillBarsType,
+  SEODataType,
+  SocialLinksType,
+  GreetingsType,
+} from './types/sections';
+
+export const greetings: GreetingsType = {
+  name: 'ComboGang',
+  title: "Hi all, I'm ComboGang",
+  description:
+    "I'm Full-Stack developer using a variety of languages to complete my projects",
+};
+
+export const openSource = {
+  githubUserName: 'combogangreal',
+};
+
+export const contact = {};
+
+export const socialLinks: SocialLinksType = {
+  github: 'https://github.com/combogangreal',
+  instagram: 'https://www.instagram.com/combogangdev/',
+  twitter: 'https://twitter.com/ComboGangDev',
+};
+
+export const skillsSection: SkillsSectionType = {
+  title: 'What I do',
+  subTitle: 'Full-Stack developer who is passionate about building great & quality products',
+  data: [
+    {
+      title: 'Full Stack Development',
+      lottieAnimationFile: '/lottie/skills/fullstack.json', // Path of Lottie Animation JSON File
+      skills: [
+        emoji(
+          '⚡ Building responsive Single-Page-Apps (SPA) & PWA in React.js'
+        ),
+        emoji('⚡ Building responsive static websites using Next.js'),
+        emoji('⚡ Build RESTful APIs using Python & Flask & Jinja2'),
+        emoji('⚡ Building responsive mobile applications using Kotlin'),
+        emoji('⚡ Building responsive desktop applications using Rust'),
+        emoji('⚡ Building advanced games using C++ & OpenGL'),
+        emoji('⚡ Building multi-purpose discord bots using Python & Discord.py'),
+      ],
+      softwareSkills: [
+        {
+          skillName: 'HTML-5',
+          iconifyTag: 'vscode-icons:file-type-html',
+        },
+        {
+          skillName: 'CSS-3',
+          iconifyTag: 'vscode-icons:file-type-css',
+        },
+        {
+          skillName: 'JavaScript',
+          iconifyTag: 'logos:javascript',
+        },
+        {
+          skillName: 'TypeScript',
+          iconifyTag: 'vscode-icons:file-type-typescript',
+        },
+        {
+          skillName: 'Reactjs',
+          iconifyTag: 'vscode-icons:file-type-reactjs',
+        },
+        {
+          skillName: 'Nextjs',
+          iconifyTag: 'vscode-icons:file-type-light-next',
+        },
+        {
+          skillName: 'Python',
+          iconifyTag: 'logos:python',
+        },
+        {
+          skillName: 'NPM',
+          iconifyTag: 'logos:npm-icon',
+        },
+        {
+          skillName: 'Yarn',
+          iconifyTag: 'logos:yarn',
+        },
+        {
+          skillName: 'Rust',
+          iconifyTag: 'logos:rust',
+        },
+        {
+          skillName: 'Go',
+          iconifyTag: 'logos:go',
+        },
+        {
+          skillName: 'Java',
+          iconifyTag: 'logos:java',
+        },
+        {
+          skillName: 'Kotlin',
+          iconifyTag: 'logos:kotlin',
+        },
+        {
+          skillName: 'C',
+          iconifyTag: 'vscode-icons:file-type-c',
+        },
+        {
+          skillName: 'CPP',
+          iconifyTag: 'vscode-icons:file-type-cpp',
+        },
+        {
+          skillName: 'OpenCV',
+          iconifyTag: 'logos:opencv',
+        },
+        {
+          skillName: 'OpenGL',
+          iconifyTag: 'logos:opengl',
+        },
+        {
+          skillName: 'Jinja',
+          iconifyTag: 'vscode-icons:file-type-jinja',
+        },
+      ],
+    },
+    {
+      title: 'Cloud Infra-Architecture',
+      lottieAnimationFile: '/lottie/skills/cloudinfra.json', // Path of Lottie Animation JSON File
+      skills: [
+        emoji('⚡ Experience of working on multiple cloud platforms'),
+        emoji(
+          '⚡ Hosting and maintaining websites on virtual machine instances along with integration of databases'
+        ),
+      ],
+      softwareSkills: [
+        // ? Check README To get icon details
+        {
+          skillName: 'AWS',
+          iconifyTag: 'logos:aws',
+        },
+        {
+          skillName: 'Heroku',
+          iconifyTag: 'logos:heroku-icon',
+        },
+        {
+          skillName: 'PostgreSQL',
+          iconifyTag: 'logos:postgresql',
+        },
+        {
+          skillName: 'Github',
+          iconifyTag: 'akar-icons:github-fill',
+        },
+        {
+          skillName: 'Docker',
+          iconifyTag: 'logos:docker-icon',
+        },
+        {
+          skillName: 'Nginx',
+          iconifyTag: 'logos:nginx',
+        },
+      ],
+    },
+  ],
+};
+
+export const SkillBars: SkillBarsType[] = [
+  {
+    Stack: 'Backend', //Insert stack or technology you have experience in
+    progressPercentage: '90', //Insert relative proficiency in percentage
+  },
+  {
+    Stack: 'Frontend/UI/UX',
+    progressPercentage: '70',
+  },
+  {
+    Stack: 'Programming',
+    progressPercentage: '60',
+  },
+];
+
+
+export const projects: ProjectType[] = [
+  {
+    name: 'Donut.rs',
+    desc: 'A donut.c clone in rust',
+    github: 'https://github.com/combogangreal/Donut.rs',
+  },
+  {
+    name: 'Port-Sniffer',
+    desc: 'A port scanner sniffer in rust',
+    github: 'https://github.com/combogangreal/Port-Sniffer',
+  },
+  {
+    name: 'Combo-Bot',
+    desc: 'A custom discord bot with buy/order system',
+    github: 'https://github.com/combogangreal/Bot',
+  }
+];
+
+// option to hide or show the ContactUs component
+export const showContactUs: boolean = true;
+
+// See object prototype on /types/section.ts page
+export const seoData: SEODataType = {
+  title: 'Combo Gang',
+  description:
+    'A passionate Full Stack Back & Front end Developer with 4 years of experience',
+  author: 'Hanzla Tauqeer',
+  image: 'https://avatars3.githubusercontent.com/u/59178380?v=4',
+  url: 'https://developer-portfolio-1hanzla100.vercel.app',
+  keywords: [
+    'Hanzla',
+    'Hanzla Tauqeer',
+    '@1hanzla100',
+    '1hanzla100',
+    'Portfolio',
+    'Hanzla Portfolio ',
+    'Hanzla Tauqeer Portfolio',
+  ],
+};
